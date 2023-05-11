125bccf915edb3a11ca627e7663396a40f22f84c
KursadEren <k.erenmaden1@outlook.com>
2023-05-11 23:42:40 +0300

    giriş sayfasının eklenmesi

---
+++ giris.php
@@ -0,0 +1,55 @@
+<!DOCTYPE html>
+<head>
+    <meta charset="utf-8">
+    <link rel="stylesheet" type="text/css" href="giris.css">
+    <link rel="stylesheet" type="text/css" href="C:\\Users\\keren\\Desktop\\web odevi\\anasayfa.css">
+</head>
+<body>
+
+
+    <header  >  
+        <div class = "ul1" >
+           <ul >
+            <li><a class ="hakkı" href="anasayfa.html">Hakkında</a></li>
+            <li><a class = "giriş" href="C:\\Users\\keren\\Desktop\\web odevi\\php\\giris.html">Giriş</a></li>
+            <li><a class ="miras" href="Miras.html">Mirasımız</a></li>
+            <li><a class = "şehir" href="sehrim.html">Şehrimiz</a></li>
+            <li><a class ="oz" href="ozgecmisim.html">Özgeçmişim</a></li> 
+            <li><a class ="ilet" href="iletisim.html">İletişim</a></li>  
+            <li><a class ="ilgi" href="C:\\Users\\keren\\Desktop\\web odevi\\public\\index.html">ilgialanlarım</a></li>   
+           </ul>
+        </div> 
+       </header> 
+       
+    <nav class = "adım2">
+        <th class="adım"> KÜRŞAD EREN MADEN </th>
+       </nav> 
+
+<div class="formlar">
+    <div class="formiçi">
+<form action="login.php" method="POST">
+<table align="center">
+<tr>
+<td>Kullanici Adi</td>
+<td>:</td>
+<td><input type="text" name="username" id="kullanıcıadi"></td>
+</tr>
+<tr>
+<td>Sifre</td>
+<td>:</td>
+<td><input type="password" name="password" id="sifre"></td>
+</tr>
+<tr>
+<td></td>
+<td></td>
+<td><input type="submit" value="Giris" id="giris"></td>
+</tr>
+</form>
+</div>
+</div>
+
+
+
+    
+</body>
+</html>
